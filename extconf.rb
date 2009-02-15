56f016f876ca52242ae34751734fa7c4ce9d7b35
Chris Mowforth <chris@mowforth.com>
2009-02-15 23:53:50 +0000

    Add cups cflags to extconf

---
+++ extconf.rb
@@ -0,0 +1,10 @@
+require "mkmf"
+
+cups_cflags = `cups-config --cflags`.chomp
+cups_libs = `cups-config --libs`.chomp
+
+with_cflags(cups_cflags) {
+  with_ldflags(cups_libs) {
+    create_makefile("cups")   
+  }
+}
--- rcups.c
+++
@@ -1,8 +0,0 @@
-#include <cups/cups.h>
-#include <ruby.h>
-
-int main (int argc, char const *argv[])
-{
-  return 0;
-}
-
