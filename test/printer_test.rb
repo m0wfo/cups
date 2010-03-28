9c6f35059f496e2102cb308dc73ebf4ad0076a79
Chris Mowforth <chris@mowforth.com>
2010-03-28 23:34:02 +0100

    Let's clean up the tests

---
+++ test/printer_test.rb
@@ -0,0 +1,7 @@
+require File.dirname(__FILE__) + '/test_helper'
+
+class PrinterTest < Test::Unit::TestCase
+  def test_something
+    # todo
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path( File.dirname(__FILE__) )
+
+require "cups"
+require "flexmock/test_unit"
