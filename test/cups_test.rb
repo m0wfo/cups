fd623d8fc0b0deaacc0d7d479b3a88f59786f7f0
Nathan Stitt <nathan@stitt.org>
2013-06-27 13:47:56 -0500

    Fixup tests for where there is no default  printer
    My machine was setup this way, and most tests failed

--- test/cups_test.rb
+++ test/cups_test.rb
@@ -20,61 +20,59 @@ class CupsTest < Test::Unit::TestCase
     assert cups_destinations.is_a?(Array)
     assert_equal cups_destinations, lplist
   end
-  
+
   def test_can_instantiate_print_job_object_with_correct_args
     assert_raise(ArgumentError) do
       Cups::PrintJob.new
     end
-    
+
     assert_nothing_raised do
       Cups::PrintJob.new("/path", @printer)
-      Cups::PrintJob.new("/path")
+      Cups::PrintJob.new("/path") if Cups.default_printer
     end
   end
-  
+
   def test_job_defaults_to_default
     assert_nothing_raised do
-      pj = Cups::PrintJob.new("/non/existent/file")
-
-      assert_equal Cups.default_printer, pj.instance_variable_get(:@printer)
+      if Cups.default_printer
+        pj = Cups::PrintJob.new( "/non/existent/file" )
+        assert_equal Cups.default_printer, pj.instance_variable_get(:@printer)
+      end
     end
   end
-  
+
   def test_we_cant_print_to_nonexistent_printers
-    assert_raise(RuntimeError) do
+    assert_raise(ArgumentError) do
       pj = Cups::PrintJob.new("/non/existent/file", "bollocks_printer")
-      
       assert !Cups.show_destinations.include?(pj.instance_variable_get(:@printer))
     end
   end
 
   def test_we_cant_print_nonexistent_files
-    pj = Cups::PrintJob.new("soft_class")
-    
+    pj = Cups::PrintJob.new("soft_class",@printer)
     assert_raise(RuntimeError) do
-      pj.print
+          pj.print
     end
-  
     assert_nil pj.job_id
   end
-  
+
   def test_we_can_pass_args_down_as_options
     options = {:foo => 'bar'}
     pj = Cups::PrintJob.new(sample, @printer, options)
     assert_equal(options, pj.job_options)
   end
-  
+
   def test_we_can_only_pass_strings_down_as_options
     options = {:foo => 'bar'}
     pj = Cups::PrintJob.new(sample, @printer, options)
     assert_raise(TypeError) { pj.print }
   end
-  
+
   def test_we_can_omit_options_and_will_set_to_empty
     pj = Cups::PrintJob.new(sample, @printer)
     assert_equal({}, pj.job_options)
   end
-  
+
   def test_print_job_cancellation
     pj = Cups::PrintJob.new(sample, @printer)
     pj.print
@@ -82,13 +80,13 @@ class CupsTest < Test::Unit::TestCase
     assert_equal pj.cancel, true
     assert pj.job_id.is_a?(Fixnum)
   end
-  
+
   def test_all_jobs_raises_with_nonexistent_printers
     assert_raise(RuntimeError) { Cups.all_jobs(nil) }
   end
-  
+
   def test_all_jobs_returns_hash
-    assert Cups.all_jobs(Cups.default_printer).is_a?(Hash)
+    assert Cups.all_jobs(@printer).is_a?(Hash)
   end
 
   def test_all_jobs_hash_contains_info_hash
@@ -98,11 +96,11 @@ class CupsTest < Test::Unit::TestCase
     assert info.is_a?(Hash)
     assert info.keys.all?{|key| [:title, :format, :submitted_by, :state, :size].include?(key)}
   end
-  
+
   def test_dest_list_returns_array
     assert Cups.show_destinations.is_a?(Array)
   end
-  
+
   def test_dest_options_returns_hash_if_real
     assert Cups.options_for(@printer).is_a?(Hash)
   end
@@ -128,7 +126,7 @@ class CupsTest < Test::Unit::TestCase
   end
 
   def test_returns_failure_string_on_cancellation
-    pj = Cups::PrintJob.new(blank_sample, @printer)
+    pj = Cups::PrintJob.new(sample, @printer)
     pj.print
 
     # assert pj.job_id == 0 # Failed jobs have an ID of zero
@@ -148,20 +146,20 @@ class CupsTest < Test::Unit::TestCase
     assert pj.state == :cancelled
     assert !pj.completed?
   end
-  
+
   def test_print_job_attributes
-    pj = Cups::PrintJob.new(sample)
+    pj = Cups::PrintJob.new(sample, @printer)
     [:printer, :filename, :job_id].each do |attr|
       assert pj.respond_to?(attr)
     end
   end
 
   private
-  
+
   def sample
     "#{File.dirname(__FILE__)}/sample.txt"
   end
-  
+
   def blank_sample
     "#{File.dirname(__FILE__)}/sample_blank.txt"
   end
