983c17df739c4f7b8d9b78c1ace7b023cbb5a346
Joiey Seeley <joiey.seeley@gmail.com>
2014-05-28 11:58:31 -0500

    testing

--- ext/ruby_cups.h
+++ ext/ruby_cups.h
@@ -2,8 +2,8 @@
 
 // st.h is needed for ST_CONTINUE constant
 #ifdef __APPLE__
-  #include <ruby/ruby.h>
-  #include <ruby/st.h>
+  #include <ruby.h>
+  #include <st.h>
 #else
   #include <ruby.h>
   #include <st.h>
